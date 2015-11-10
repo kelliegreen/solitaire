8054fde6702a0fbeb18243c6a1e5d762a2738ec1
Kellie Green <kelliegreen10690@gmail.com>
2015-11-09 17:03:54 -0700

    added all 7 rows

--- app/deck/deckCtrl.js
+++ app/deck/deckCtrl.js
@@ -7,11 +7,15 @@ angular.module('solitaire').controller('deckCtrl', function ($scope, deckService
 		deckService.getDeck().then(function (response) {
 			$scope.deckId = response.data.deck_id;
 			// console.log($scope.deckId);
-			deckService.firstPile($scope.deckId).then(function (response) {
-				$scope.pileOne = response.data.cards[0].image;
-			});
-			deckService.secondPile($scope.deckId).then(function(response) {
-				$scope.pileTwo = response.data.cards[0, 1].image;
+			// deckService.firstPile($scope.deckId).then(function (response) {
+			// 	$scope.pileOne = response.data.cards[0].image;
+			// });
+			// deckService.secondPile($scope.deckId).then(function (response) {
+			// 	$scope.pileTwo = response.data.cards[0, 1].image;
+			// 	// console.log(response.data);
+			// });
+			deckService.drawCard($scope.deckId).then(function(response) {
+				$scope.row7 = response.data.cards[0].image;
 			});
 		});
 	};
@@ -25,9 +29,10 @@ angular.module('solitaire').controller('deckCtrl', function ($scope, deckService
 			$scope.cardNum = response.data.remaining;
 			console.log($scope.cardNum);
 			if ($scope.cardNum === 0) {
-				deckService.shuffle($scope.deckId).then(function (response) {
+				deckService.shuffle($scope.deckId, $scope.cardNum).then(function (response) {
 					$scope.cardShuffle = response;
-					// console.log(response);
+					$scope.response.data.remaining = $scope.cardNum;
+					// console.log(response.data.remaining);
 				});
 			}
 		});
